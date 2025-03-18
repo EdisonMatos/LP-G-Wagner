2ed3dcefc698ca899a8244f9867a9a94519d1d65
Edison Matos <edison.matos@live.com>
2025-03-18 14:31:05 -0300

    feat: Ajuste de responsividade do título da hero

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -127,7 +127,7 @@ export default function Hero({ appDownloadButtons, personImg, fundoImage }) {
                     </div>
                   </MotionDivDownToUp>
                   <MotionDivDownToUp>
-                    <div className="text-white flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7">
+                    <div className="text-white flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[46px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7 desktop1:text-title5">
                       <h1 className="">{content.texts.hero.title}</h1>
                     </div>
                   </MotionDivDownToUp>
