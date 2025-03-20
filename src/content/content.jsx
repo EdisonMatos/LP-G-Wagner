d1d3b24c300dbbde2e4ebfdaab12067a8390d03d
Edison Matos <edison.matos@live.com>
2025-03-20 11:46:54 -0300

    feat: Corrige endereço.jsx

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -58,8 +58,7 @@ export const infos = {
       <p>Endereço:</p>
       <p>Av. Engenheiro Domingos Ferreira, nº 1195</p>
       <p>Caixa postal 259, Bairro Boa Viagem</p>
-      <p> Recife - PE</p>
-      <p>CEP 51111-020</p>
+      <p>Recife - PE, CEP 51111-020</p>
     </div>
   ),
   expediente: (
